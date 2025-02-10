04ba8f8fa34b10e35b7d553cc4767d9624120065
Kyle <kylewhite369@gmail.com>
2025-02-11 10:02:50 +1300

    product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,21 +1,18 @@
 import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
-import { useUserStore } from "../stores/useUserStores";
+import { useUserStore } from "../stores/useUserStore";
 import { useCartStore } from "../stores/useCartStore";
 
-
 const ProductCard = ({ product }) => {
-
 	const { user } = useUserStore();
 	const { addToCart } = useCartStore();
-
 	const handleAddToCart = () => {
 		if (!user) {
 			toast.error("Please login to add products to cart", { id: "login" });
 			return;
 		} else {
+			// add to cart
 			addToCart(product);
-		
 		}
 	};
 
