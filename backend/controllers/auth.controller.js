ff5744dd00ffabc6ce96f72965742592f7ce7d43
Kyle <kylewhite369@gmail.com>
2025-02-11 09:39:56 +1300

    login status updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -85,11 +85,11 @@ export const login = async (req, res) => {
                 role: user.role,
             })
         } else {
-            res.status(401).json({ message: "Invalid email or password" });
+            res.status(400).json({ message: "Invalid email or password" });
         }
     } catch (error) {
         console.log("Error in login controller", error.message);
-        res.status(400).json({ message: "Invalid credentials" });
+        res.status(401).json({ message: "Invalid credentials" });
     }
 };
 
