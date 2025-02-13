bc590d3418a3e7e68de3a8fd0b01090fbe63f5a5
Kyle <kylewhite369@gmail.com>
2025-02-14 12:57:00 +1300

    Project Completion

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51QmlQ2HycoQaGHpO3dfYAysDuYtPg18dEvM58u71btSkL6BQsVV5tdOfXxknTCDGVClePu31pQJBUJeefhsKPPwm00EfLmxB7I"
 );
 
 const OrderSummary = () => {
