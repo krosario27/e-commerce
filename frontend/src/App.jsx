c9cd2b97beb225c2f240e4c5eae9dc3fda38dc04
Kyle <kylewhite369@gmail.com>
2025-02-18 09:54:00 +1300

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,9 +39,8 @@ function App() {
       {/* Background gradient */}
 			<div className='absolute inset-0 overflow-hidden'>
 				<div className='absolute inset-0'>
-					<div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-
-          [radial-gradient(ellipse_at_top,_rgba(16,185,129,0.3)_0%,_rgba(10,80,60,0.2)_45%,
-          _rgba(0,0,0,0.1)_100%)]' />
+					<div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full 
+          bg-[radial-gradient(ellipse_at_top,_rgba(99,102,241,0.3)_0%,_rgba(55,65,81,0.2)_45%,_rgba(0,0,0,0.1)_100%)]' />
 				</div>
 			</div>
 
